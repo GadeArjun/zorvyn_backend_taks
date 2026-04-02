3187125d426b2e0a5b89266688e17054cf545e19
gadearjun244 <gadearjun244@gmail.com>
2026-04-02 18:22:25 +0000

    add .env.example file add express rate limiter

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 const { connectDB } = require("./config/db");
 const { authRouter } = require("./routes/auth.routes");
 const { userRouter } = require("./routes/user.routes");
 const { transactionRouter } = require("./routes/transaction.routes");
-require("dotenv").config();
 
 const swaggerUi = require("swagger-ui-express");
 const { swaggerSpec } = require("./config/swagger");
 
+// general API limiter
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 150, // 150 req
+  message: {
+    success: false,
+    message: "Too many requests, please try again later.",
+  },
+  standardHeaders: true, // return rate limit info in headers
+  legacyHeaders: false,
+});
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.set("trust proxy", 1);
+app.use(apiLimiter);
+
 connectDB();
 
 // auth route
-app.use("/api/auth", authRouter);
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 20, // only 10 requests (login/register)
+  message: {
+    success: false,
+    message: "Too many login /sigup attempts. Try again later.",
+  },
+  standardHeaders: true, // return rate limit info in headers
+  legacyHeaders: false,
+});
+
+app.use("/api/auth", authLimiter, authRouter);
 
 //user route
 app.use("/api/user", userRouter);
